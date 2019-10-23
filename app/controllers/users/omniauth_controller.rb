6e6550a5dfc6e886dec84b36ab2903dc22ff9599
Ryu Sato <ryu@weseek.co.jp>
2019-10-24 01:30:13 +0900

    add google to oauth provider

--- app/controllers/users/omniauth_controller.rb
+++ app/controllers/users/omniauth_controller.rb
@@ -1,18 +1,28 @@
 class Users::OmniauthController < Devise::OmniauthCallbacksController
   def facebook
-    @user = User.find_or_create_for_oauth(request.env['omniauth.auth'])
+    find_or_create_user_and_login("Facebook")
+  end
 
-    if @user.persisted?
-      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-      sign_in_and_redirect @user, event: :authentication
-    else
-      set_flash_message(:alert, :failure, kind: "Facebook", reason: 'User not found and cannot register')
-      session["devise.facebook_data"] = request.env['omniauth.auth']
-      redirect_to new_user_session_path
-    end
+  def google_oauth2
+    find_or_create_user_and_login("Google")
   end
 
   def failure
     redirect_to new_user_session_path
+  end
+
+  private
+
+  def find_or_create_user_and_login(oauth_kind)
+    @user = User.find_or_create_for_oauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      set_flash_message(:notice, :success, kind: oauth_kind) if is_navigational_format?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      set_flash_message(:alert, :failure, kind: oauth_kind, reason: 'User not found and cannot register')
+      session["devise.facebook_data"] = request.env['omniauth.auth']
+      redirect_to new_user_session_path
+    end
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -261,6 +261,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"], provider_ignores_state: true
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
